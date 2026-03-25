aacfbfac7d67d5919c81e727d340af4ef4664876
mariaheb2 <Mariah.Pereira0724@jala.university>
2026-03-25 10:11:11 -0300

    Update- Adding initial entities modeling

--- cards_against_humanity_server/src/main/java/cards_against_humanity/Main.java
+++ cards_against_humanity_server/src/main/java/cards_against_humanity/Main.java
@@ -1,4 +1,4 @@
-package redes;
+package cards_against_humanity;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
---
+++ cards_against_humanity_server/src/main/java/cards_against_humanity/models/User.java
@@ -0,0 +1,8 @@
+package cards_against_humanity.models;
+
+public class User {
+    private String id;
+    private String username;
+    private String email;
+    private String passwordHash;
+}
